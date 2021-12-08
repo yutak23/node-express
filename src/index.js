d1a918329f9568beda0e9345f1b339230153ed61
Yuta Katayama <yutapilafu@gmail.com>
2021-12-08 21:17:37 +0900

    prettierの設定（useTabs=true）

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ import express from 'express';
 const app = express();
 
 app.get('/', async (req, res) => {
-  const reqTime = Date.now();
-  console.log(Array.from('foo'));
+	const reqTime = Date.now();
+	console.log(Array.from('foo'));
 
-  await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('sleep');
-    }, 500);
-  });
+	await new Promise((resolve) => {
+		setTimeout(() => {
+			resolve('sleep');
+		}, 500);
+	});
 
-  res.status(200).send({
-    msg: 'hello world!',
-    elaptime: Date.now() - reqTime,
-  });
+	res.status(200).send({
+		msg: 'hello world!',
+		elaptime: Date.now() - reqTime,
+	});
 });
 
 app.listen(3000, () => console.log('listening on port 3000!'));
--- webpack.config.js
+++ webpack.config.js
@@ -3,28 +3,28 @@ const nodeExternals = require('webpack-node-externals');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-  target: 'node',
-  externals: [nodeExternals()],
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  name: 'node-express',
-  entry: {
-    index: './src/index.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-    ],
-  },
-  plugins: [new ESLintPlugin({ exclude: 'node_modules' })],
+	target: 'node',
+	externals: [nodeExternals()],
+	mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+	name: 'node-express',
+	entry: {
+		index: './src/index.js',
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].js',
+		clean: true,
+	},
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+		],
+	},
+	plugins: [new ESLintPlugin({ exclude: 'node_modules' })],
 };
