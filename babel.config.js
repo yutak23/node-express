a8110d46203a96a712fa11f48c92cde2bf7c0a23
Yuta Katayama <yutapilafu@gmail.com>
2022-05-19 21:29:22 +0900

    babel.config.jsに今更ながらコメントを追加

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
+/**
+ * targets: { node: 'current' } <- https://babeljs.io/docs/en/options#targetsnode
+ * current = process.versions.node である
+ *
+ * ※nodeに変換するのであれば、`useBuiltIns`・`corejs`の設定は不要かもしれない（nodeではasync/awaitも標準組み込みオブジェクトも対応しているので）
+ *   ES5に変換したい時は必要なので一応残しておく
+ */
 module.exports = {
 	presets: [
 		[
