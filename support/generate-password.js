4f4da20c7fd3d396375f715c167c4150b72d28e2
Yuta Katayama <yutapilafu@gmail.com>
2022-06-21 20:43:54 +0900

    feat: 裏からデータを登録したい時のためのパスワード生成CLIを作成

---
+++ support/generate-password.js
@@ -0,0 +1,19 @@
+const bcrypt = require('bcrypt');
+const { program } = require('commander');
+
+program
+	.version('1.0.0')
+	.requiredOption(
+		'-p, --password <password>',
+		'specify the password you want to convert'
+	)
+	.parse(process.argv);
+
+const options = program.opts();
+
+const encrypt = (v) => bcrypt.hashSync(v, 10);
+
+console.log({
+	input: options.password,
+	'bcrypt-hash': encrypt(options.password)
+});
